d5e0949d4eded8dc5af6d0c35fbda42b2f40d18a
Daann26 <132864922+Daann26@users.noreply.github.com>
2024-06-11 13:37:46 -0400

    cramos una aplicacion

---
+++ src/componentes/Campo/campo.js
@@ -0,0 +1,25 @@
+import "./campo.css"
+
+
+const Campo = (props) =>{
+    
+    const placeHolderModificado = `${props.placeholder}...`
+
+    const{ type="text" } = props
+    
+    const manejarCambio = (e) =>{
+        props.actualizarValor(e.target.value)
+    }
+    return <div className={`campo campo-${type}`}>
+    <label>{props.titulo}</label>
+    <input 
+    placeholder={placeHolderModificado} 
+    required={props.required}
+    value={props.valor}
+    onChange={manejarCambio}
+    type={type}
+    />
+    </div>
+}
+
+export default Campo
